74c0d73e7fbf66cd8d0999751c4c84b91f4bd915
Subash <sapkotasubash784@gmail.com>
2026-07-23 14:02:57 +0200

    ss

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "learning_logs",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+"""django settings for learning_log project."""
+
+#My setttings 
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = 'learning_logs:index'
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #Code wise a model is just a class, it has attribute and methods, just like every class we have discussed. 
@@ -6,11 +7,22 @@ class Topic (models.Model):
       """A topic the user is learning about"""
       text = models.CharField(max_length=200)
       date_added = models.DateTimeField(auto_now_add=True)  
+      owner=models.ForeignKey(User, on_delete=models.CASCADE)
 
-def __str__(self):
-      """Return a string representation of the model."""
-      return self.text
+      def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
 
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return self.text[:50] + "..."
 
 
